f555338535a7b4259cf16ad94603b33f683efc1c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-31 11:27:31 +0300

    Added the onSavePostClicked function for the component

--- components/Posts/AddPostForm.tsx
+++ components/Posts/AddPostForm.tsx
@@ -1,14 +1,28 @@
+import { useAppDispatch } from '@/Redux/hooks'
 import React, { ChangeEvent, useState } from 'react'
+import { nanoid } from '@reduxjs/toolkit'
+import { postAdded } from '@/Redux/features/posts/postsSlice'
 
 const AddPostForm = () => {
 	const [title, setTitle] = useState('')
 	const [content, setContent] = useState('')
+
+	const dispatch = useAppDispatch()
 
 	const onTitleChanged = (event: ChangeEvent<HTMLInputElement>) =>
 		setTitle(event.target.value)
 
 	const onContextChanged = (e: ChangeEvent<HTMLInputElement>) =>
 		setContent(e.target.value)
+
+	const onSavePostClicked = () => {
+		if (title && content) {
+			dispatch(postAdded({ id: nanoid(), title, content }))
+
+			setTitle('')
+			setContent('')
+		}
+	}
 
 	return (
 		<section>
