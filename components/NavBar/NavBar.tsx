a6ffa4c4a62d0c7856c9ab00a8e0eb69d8568b60
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-31 10:03:42 +0300

    Added some styles for the header fo the app

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import ThemeToggle from './ThemeToggle'
+import Link from 'next/link'
 
 type Props = {}
 
 const NavBar = (props: Props) => {
 	return (
-		<nav className='h-[10vh] w-screen shadow-sm dark:shadow-zinc-800'></nav>
+		<nav className='h-[10vh] w-screen shadow-sm dark:shadow-zinc-800 flex flex-row justify-between px-10'>
+			<div className='flex items-center'>
+				<Link href='/'>
+					<h1 className='uppercase font-bold text-2xl text-zinc-600'>
+						Redux Toolkit Refresher
+					</h1>
+				</Link>
+			</div>
+			<div className=''></div>
+			<div className='flex items-center'>
+				<ThemeToggle />
+			</div>
+		</nav>
 	)
 }
 
