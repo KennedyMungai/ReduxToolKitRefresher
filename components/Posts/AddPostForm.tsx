3d1b5ad82038846539ded2028b590cebb6a50cf6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-31 11:40:21 +0300

    Made some changes for the AddPostForm component

--- components/Posts/AddPostForm.tsx
+++ components/Posts/AddPostForm.tsx
@@ -1,7 +1,9 @@
+'use client'
 import { useAppDispatch } from '@/Redux/hooks'
 import React, { ChangeEvent, useState } from 'react'
 import { nanoid } from '@reduxjs/toolkit'
 import { postAdded } from '@/Redux/features/posts/postsSlice'
+import { Button } from '../ui/button'
 
 const AddPostForm = () => {
 	const [title, setTitle] = useState('')
@@ -12,8 +14,9 @@ const AddPostForm = () => {
 	const onTitleChanged = (event: ChangeEvent<HTMLInputElement>) =>
 		setTitle(event.target.value)
 
-	const onContextChanged = (e: ChangeEvent<HTMLInputElement>) =>
-		setContent(e.target.value)
+	const onContextChanged = (
+		e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>
+	) => setContent(e.target.value)
 
 	const onSavePostClicked = () => {
 		if (title && content) {
@@ -25,29 +28,32 @@ const AddPostForm = () => {
 	}
 
 	return (
-		<section>
+		<section className='w-screen h-[90vh] bg-zinc-100 dark:bg-zinc-800 p-5 rounded-md flex flex-col justify-center items-center gap-2'>
 			<h2>Add a new post</h2>
-			<form>
-				<label htmlFor='postTitle'>Post Title</label>
-				<input
-					type='text'
-					id='postTitle'
-					name='postTitle'
-					value={title}
-					onChange={onTitleChanged}
-				/>
-				<label htmlFor='postContent'>Content:</label>
-				<input
-					type='text'
-					id='postContent'
-					name='postContent'
-					value={content}
-					onChange={onContextChanged}
-				/>
-				<button type='submit' onClick={onSavePostClicked}>
-					Save Button
-				</button>
+			<form className='flex flex-col gap-2'>
+				<div className=''>
+					<label htmlFor='postTitle'>Post Title: </label>
+					<input
+						type='text'
+						id='postTitle'
+						name='postTitle'
+						value={title}
+						onChange={onTitleChanged}
+					/>
+				</div>
+				<div className=''>
+					<label htmlFor='postContent'>Content: </label>
+					<textarea
+						id='postContent'
+						name='postContent'
+						value={content}
+						onChange={onContextChanged}
+					/>
+				</div>
 			</form>
+			<Button type='submit' onClick={onSavePostClicked}>
+				Save Button
+			</Button>
 		</section>
 	)
 }
