523caad6c1a028caec714634fb27d63e724ec16b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-31 09:37:38 +0300

    Created the NavBar component

---
+++ components/NavBar/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const NavBar = (props: Props) => {
+	return (
+		<nav className='h-[10vh] w-screen shadow-sm dark:shadow-zinc-800'></nav>
+	)
+}
+
+export default NavBar
