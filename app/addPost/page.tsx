445163edd9bba12d8ce12b699cb3e9a72a3a1f7c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-31 11:39:53 +0300

    Created the addPost route

---
+++ app/addPost/page.tsx
@@ -0,0 +1,10 @@
+import AddPostForm from '@/components/Posts/AddPostForm'
+import React from 'react'
+
+type Props = {}
+
+const AddPostPage = (props: Props) => {
+	return <AddPostForm />
+}
+
+export default AddPostPage
