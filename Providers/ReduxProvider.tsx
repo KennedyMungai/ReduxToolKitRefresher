58494aee610f4246e829bdbeb85c6506cf6e5860
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-31 10:11:52 +0300

    Created the ReduxProvider for the app

---
+++ Providers/ReduxProvider.tsx
@@ -0,0 +1,13 @@
+import store from '@/Redux/store'
+import React, { ReactNode } from 'react'
+import { Provider } from 'react-redux'
+
+type Props = {
+	children: ReactNode
+}
+
+const ReduxProvider = ({ children }: Props) => {
+	return <Provider store={store}>{children}</Provider>
+}
+
+export default ReduxProvider
