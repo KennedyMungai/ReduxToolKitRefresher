2cd3dfd4fc3db8f82f284cbeb3000df82b035902
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-31 10:18:32 +0300

    Created the counterSlice

---
+++ Redux/features/counter/counterSlice.ts
@@ -0,0 +1,25 @@
+import { RootState } from '@/Redux/store'
+import { createSlice } from '@reduxjs/toolkit'
+
+interface IState {
+    count: number
+}
+
+const initialState: IState = {
+    count: 0
+}
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment: (state) => { state.count++ },
+        decrement: (state) => { state.count-- },
+        incrementByAmount: (state, action) => { state.count += action.payload }
+    }
+})
+
+export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const selectCount = (state: RootState) => state.count
+
+export default counterSlice.reducer
